0c11ea8b80f0c4e7d759ba0cc289b9ecb076e04b
ssspj <143371137+ssspj@users.noreply.github.com>
2023-11-12 22:31:09 +0900

    Add files via upload

---
+++ ListViewItem.java
@@ -0,0 +1,28 @@
+package com.example.directions.ListViewFunction;
+
+public class ListViewItem {
+
+  String POIStr ;
+  String AddressStr ;
+  double POIlat;
+  double POIlon;
+
+  public void setPOIStr(String title) {
+    POIStr = title ;
+  }
+  public void setAddressStr(String desc) {
+    AddressStr = desc ;
+  }
+  public String getPOIStr() {
+    return this.POIStr ;
+  }
+  public String getAddressStr() {
+    return this.AddressStr ; }
+  public  void setLat(double lat){
+    POIlat = lat;
+  }
+  public  void setLon(double lon){
+    POIlon = lon;
+  }
+
+}
